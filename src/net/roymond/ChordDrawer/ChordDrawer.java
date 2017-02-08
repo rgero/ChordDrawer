4a7e1d4fc0343ea94dc2e7372eedc78eaea2a242
Roy Gero <roy.gero.ii@gmail.com>
2017-02-08 15:47:08 -0500

    Started to work on drawing the to the screen. Right now it is just drawing the points. I'll need to figure out a way to draw the lines.

--- src/net/roymond/ChordDrawer/ChordDrawer.java
+++ src/net/roymond/ChordDrawer/ChordDrawer.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * The Chord Drawer base class.
@@ -21,20 +23,48 @@ public class ChordDrawer {
     private BufferedImage chordImg;
     private Graphics2D chordGraphic;
 
+    private int numberOfStrings;
+    private int numberOfFrets;
+    private int horizontalPadding;
+    private int verticalPadding;
+
+    private List<Point> intersectionPoints;
+
     private void createBaseImage(){
         chordImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         chordGraphic = chordImg.createGraphics();
         chordGraphic.setColor(Color.black);
         chordGraphic.fillRect(0,0,width, height);
 
+        int workingWidth = width-2*horizontalPadding;
+        int workingHeight = height - 2 * verticalPadding;
+
         //Here would be where we draw the guitar.
+        for(int string = 0; string < numberOfStrings; string++){
+            for(int fret = 0; fret < numberOfFrets; fret++){
+                int xPos = horizontalPadding + string * (workingWidth/(numberOfStrings-1));
+                int yPos = verticalPadding + fret * (workingHeight/(numberOfFrets-1));
+                System.out.println(String.format("x: %d\ty: %d", xPos, yPos));
+                intersectionPoints.add( new Point(xPos,yPos) );
+                chordImg.setRGB(xPos,yPos, Color.red.getRGB());
+            }
+        }
+
+
     }
 
     ChordDrawer(){
 
+        numberOfStrings = 6;
+        numberOfFrets = 5;
+
         width = 340;
         height = 416;
+        horizontalPadding = 20;
+        verticalPadding = 8;
+
         chordImage.setText("");
+        intersectionPoints = new ArrayList<>();
 
         createBaseImage();
 
